496a0109b05257753c97d936b55928a3f29bfda9
Kelsang Yonjan <kyonjan@mcneese.com>
2025-09-27 23:18:50 -0500

    The first commit from local !!

---
+++ src/main/java/ToolsQA/DemoMavenEclipseProject/App.java
@@ -0,0 +1,13 @@
+package ToolsQA.DemoMavenEclipseProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
